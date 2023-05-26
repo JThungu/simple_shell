51b04aeec3e2424a42cc086e5f831cf9d7bd5ab6
calebrori <32507245+calebrori@users.noreply.github.com>
2023-05-27 02:19:48 +0300

    vars.c

--- vars.c
+++ vars.c
@@ -2,8 +2,8 @@
 
 /**
  * is_chain - test if current char in buffer is a chain delimeter.
- * @pop: the parameter struct
- * @buf: the char buffer
+ * @pop: parameter structure for prototype
+ * @buf: char buffer
  * @p: address of current position in buf
  *
  * Return: 1 if chain delimeter, 0 otherwise
@@ -37,8 +37,8 @@ int is_chain(pops *pop, char *buf, size_t *p)
 
 /**
  * check_chain - checks we should continue chaining based on last status
- * @pop: the parameter struct
- * @buf: the char buffer
+ * @pop: parameter structure for prototype
+ * @buf: char buffer
  * @p: address of current position in buf
  * @i: starting position in buf
  * @len: length of buf
@@ -71,7 +71,7 @@ void check_chain(pops *pop, char *buf, size_t *p, size_t i, size_t len)
 
 /**
  * replace_alias - replaces an aliases in the tokenized string
- * @pop: the parameter struct
+ * @pop: parameter structure for prototype
  *
  * Return: 1 if replaced, 0 otherwise
  */
@@ -100,7 +100,7 @@ int replace_alias(pops *pop)
 
 /**
  * replace_vars - replaces vars in the tokenized string
- * @pop: the parameter struct
+ * @pop: parameter structure for prototype
  *
  * Return: 1 if replaced, 0 otherwise
  */
