67a56e7ed4d2798936538d59576b4b2ca0f59968
JThungu <josephkangethe848@gmail.com>
2023-05-26 13:11:39 -0700

    js

--- _shell_9.c
+++ _shell_9.c
@@ -6,13 +6,14 @@
  */
 size_t list_len(const list_t *h)
 {
-        size_t i = 0;
-        while (h)
-        {
-                h = h->next;
-                i++;
-        }
-        return (i);
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
 /**
  * list_to_strings - returns an array of str of the list->str
@@ -21,30 +22,31 @@ size_t list_len(const list_t *h)
  */
 char **list_to_strings(list_t *head)
 {
-        list_t *node = head;
-        size_t i = list_len(head), j;
-        char **strs;
-        char *str;
-        if (!head || !i)
-                return (NULL);
-        strs = malloc(sizeof(char *) * (i + 1));
-        if (!strs)
-                return (NULL);
-        for (i = 0; node; node = node->next, i++)
-        {
-                str = malloc(_strlen(node->str) + 1);
-                if (!str)
-                {
-                        for (j = 0; j < i; j++)
-                                free(strs[j]);
-                        free(strs);
-                        return (NULL);
-                }
-                str = _strcpy(str, node->str);
-                strs[i] = str;
-        }
-        strs[i] = NULL;
-        return (strs);
+	list_t *node = head;
+	size_t i = list_len(head), j;
+	char **strs;
+	char *str;
+
+	if (!head || !i)
+		return (NULL);
+	strs = malloc(sizeof(char *) * (i + 1));
+	if (!strs)
+		return (NULL);
+	for (i = 0; node; node = node->next, i++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (j = 0; j < i; j++)
+				free(strs[j]);
+			free(strs);
+			return (NULL);
+		}
+		str = _strcpy(str, node->str);
+		strs[i] = str;
+	}
+	strs[i] = NULL;
+	return (strs);
 }
 /**
  * print_list - prints all elements of a list_t linked list
@@ -53,18 +55,19 @@ char **list_to_strings(list_t *head)
  */
 size_t print_list(const list_t *h)
 {
-        size_t i = 0;
-        while (h)
-        {
-                _puts(convert_number(h->num, 10, 0));
-                _putchar(':');
-                _putchar(' ');
-                _puts(h->str ? h->str : "(nil)");
-                _puts("\n");
-                h = h->next;
-                i++;
-        }
-        return (i);
+	size_t i = 0;
+
+	while (h)
+	{
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
 /**
  * node_starts_with - returns node whose str starts with prefix
@@ -76,15 +79,16 @@ size_t print_list(const list_t *h)
  */
 list_t *node_starts_with(list_t *node, char *prefix, char c)
 {
-        char *p = NULL;
-        while (node)
-        {
-                p = starts_with(node->str, prefix);
-                if (p && ((c == -1) || (*p == c)))
-                        return (node);
-                node = node->next;
-        }
-        return (NULL);
+	char *p = NULL;
+
+	while (node)
+	{
+		p = starts_with(node->str, prefix);
+		if (p && ((c == -1) || (*p == c)))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
 }
 /**
  * get_node_index - gets the index of a node
@@ -94,14 +98,14 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
  */
 ssize_t get_node_index(list_t *head, list_t *node)
 {
-        size_t i = 0;
-        while (head)
-        {
-                if (head == node)
-                        return (i);
-                head = head->next;
-                i++;
-        }
-        return (-1);
-}
+	size_t i = 0;
 
+	while (head)
+	{
+		if (head == node)
+			return (i);
+		head = head->next;
+		i++;
+	}
+	return (-1);
+}
--- error.c
+++ error.c
@@ -6,14 +6,15 @@
  */
 void _eputs(char *str)
 {
-        int i = 0;
-        if (!str)
-                return;
-        while (str[i] != '\0')
-        {
-                _eputchar(str[i]);
-                i++;
-        }
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
 }
 /**
  * _eputchar - writes the char c to stderr
@@ -24,16 +25,17 @@ void _eputs(char *str)
  */
 int _eputchar(char c)
 {
-        static int i;
-        static char buf[WRITE_BUF_SIZE];
-        if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-        {
-                write(2, buf, i);
-                i = 0;
-        }
-        if (c != BUF_FLUSH)
-                buf[i++] = c;
-        return (1);
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
 /**
  * _putfd - writes the char c to given fd
@@ -45,16 +47,17 @@ int _eputchar(char c)
  */
 int _putfd(char c, int fd)
 {
-        static int i;
-        static char buf[WRITE_BUF_SIZE];
-        if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-        {
-                write(fd, buf, i);
-                i = 0;
-        }
-        if (c != BUF_FLUSH)
-                buf[i++] = c;
-        return (1);
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
 /**
  * _putsfd - prints an input string
@@ -65,12 +68,13 @@ int _putfd(char c, int fd)
  */
 int _putsfd(char *str, int fd)
 {
-        int i = 0;
-        if (!str)
-                return (0);
-        while (*str)
-        {
-                i += _putfd(*str++, fd);
-        }
-        return (i);
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
 }
