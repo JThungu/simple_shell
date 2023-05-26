2308e8d5b9af6a9eb9a35fe417d96c80353eee1f
calebrori <32507245+calebrori@users.noreply.github.com>
2023-05-27 01:22:33 +0300

    checker.c

--- checker.c
+++ checker.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * _strlen - returns string length
+ * @s: length string to inspect
  *
  * Return: integer length of string
  */
@@ -19,9 +19,9 @@ int _strlen(char *s)
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs
- * @s1: the first strang
- * @s2: the second strang
+ * _strcmp - lexicogarphic comparison of two strangs initiated
+ * @s1: first string
+ * @s2: second string
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
@@ -41,9 +41,9 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * starts_with - checks if needle starts with haystack
+ * starts_with - scans needle in haystack
  * @haystack: string to search
- * @needle: the substring to find
+ * @needle: substring to find
  *
  * Return: address of next char of haystack or NULL
  */
@@ -57,8 +57,8 @@ char *starts_with(const char *haystack, const char *needle)
 
 /**
  * _strcat - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
+ * @dest: buffer destination 
+ * @src: buffer source 
  *
  * Return: pointer to destination buffer
  */
