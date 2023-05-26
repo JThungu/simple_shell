e1cd538b72be2ab70c720ea8067f1250d73c5c82
calebrori <32507245+calebrori@users.noreply.github.com>
2023-05-27 00:27:04 +0300

    getinfo.c

---
+++ getinfo.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * clear_pop - initializes pops struct.
+ * @pop: struct address
+ */
+void clear_pop(pops *pop)
+{
+	pop->arg = NULL;
+	pop->argv = NULL;
+	pop->path = NULL;
+	pop->argc = 0;
+}
+
+/**
+ * set_pop - initializes pops struct
+ * @pop: struct address
+ * @av: argument vector
+ */
+void set_pop(pops *pop, char **av)
+{
+	int i = 0;
+
+	pop->fname = av[0];
+	if (pop->arg)
+	{
+		pop->argv = strtow(pop->arg, " \t");
+		if (!pop->argv)
+		{
+
+			pop->argv = malloc(sizeof(char *) * 2);
+			if (pop->argv)
+			{
+				pop->argv[0] = _strdup(pop->arg);
+				pop->argv[1] = NULL;
+			}
+		}
+		for (i = 0; pop->argv && pop->argv[i]; i++)
+			;
+		pop->argc = i;
+
+		replace_alias(pop);
+		replace_vars(pop);
+	}
+}
+
+/**
+ * free_pop - frees pops struct fields
+ * @pop: struct address
+ * @all: true if freeing all fields
+ */
+void free_pop(pops *pop, int all)
+{
+	ffree(pop->argv);
+	pop->argv = NULL;
+	pop->path = NULL;
+	if (all)
+	{
+		if (!pop->cmd_buf)
+			free(pop->arg);
+		if (pop->env)
+			free_list(&(pop->env));
+		if (pop->history)
+			free_list(&(pop->history));
+		if (pop->alias)
+			free_list(&(pop->alias));
+		ffree(pop->environ);
+			pop->environ = NULL;
+		bfree((void **)pop->cmd_buf);
+		if (pop->readfd > 2)
+			close(pop->readfd);
+		_putchar(BUF_FLUSH);
+	}
+}
